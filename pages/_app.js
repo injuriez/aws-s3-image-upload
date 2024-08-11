bb3393cc0d3b097891b07eb3192c6bdbc8b8ede0
ovrkast <kastt242@gmail.com>
2024-08-10 22:11:56 -0500

    f

--- pages/_app.js
+++ pages/_app.js
@@ -7,9 +7,7 @@ import Navbar from '../components/navbar';
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
+
       <div className="flex flex-col">
         <Navbar />
         <Component {...pageProps} />
